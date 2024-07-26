9a41c92ad35191abb4ca8b3ea86db5ac53171368
Alexander <Alexander29_90@icloud.com>
2024-07-26 12:30:30 +0500

    Added Labels and prepare for segue

--- TabBarApp/StartViewController.swift
+++ TabBarApp/StartViewController.swift
@@ -9,5 +9,15 @@ import UIKit
 
 final class StartViewController: UIViewController {
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let tabBarController = segue.destination as? UITabBarController
+        tabBarController?.viewControllers?.forEach{ viewController in
+            if let firstVS = viewController as? FirstViewController {
+                firstVS.view.backgroundColor = .systemOrange
+            } else if let secondVC = viewController as? SecondViewController {
+                secondVC.view.backgroundColor = .systemMint
+            }
+        }
+    }
 }
 
